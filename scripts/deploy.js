f5bac7cdc303b243a43585f17ee8b1945cf4e932
jorge <geolg763@gmail.com>
2022-08-11 17:04:26 -0500

    build a ERC721 and make tests

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+
+  const SapoPerro = await hre.ethers.getContractFactory("SapoPerroCollection");
+  const sapoPerro = await SapoPerro.deploy();
+
+  await sapoPerro.deployed();
+
+  console.log("sapoPerro deployed to:", sapoPerro.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
